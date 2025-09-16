34e8ca302eae49756cdea36281658664edf6a2df
tobiasabasdev <tobiasabasdev@gmail.com>
2025-09-16 15:09:44 -0300

    responsive

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        itemsH3.forEach(el => el.style.color = "white")
+        itemsH3.forEach(el => el.style.color = "goldenrod")
         itemsP.forEach(el => el.style.color = "white")
         document.body.style.backgroundColor = "black";
       } else {
@@ -19,3 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   observer.observe(sectionClientes);
 });
+
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+    sidebar.classList.remove('hide');
+    sidebar.classList.add('show');
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('show');
+    sidebar.classList.add('hide');
+    sidebar.addEventListener('animationend', function handler() {
+        if (sidebar.classList.contains('hide')) {
+            sidebar.style.display = "none";
+        }
+        sidebar.removeEventListener('animationend', handler);
+    });
+}
+
